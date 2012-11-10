d33f6f9c8a7493f23c3fb72da08044bb72a3adb0
oddjobsman <oddjobsman@radeonsys.com>
2012-11-10 09:36:59 +0530

    Remove dead code from Category model <kini>

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,7 @@ class Category
   include Mongoid::Paperclip
 
   field :name, :type => String
-  #field :badge_image_url, :type => String, :default => "http://i420.photobucket.com/albums/pp282/jerrad_09/icon_badge.gif"
+
   has_mongoid_attached_file :badge_image
+
 end
